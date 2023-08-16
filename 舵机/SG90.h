8b580f5431cfb2df2639823bf23f86cac178c1c7
Nino <121025566+NinoC137@users.noreply.github.com>
2023-08-16 17:37:16 +0800

    更新部分外设库

--- 舵机/SG90.h
+++ 舵机/SG90.h
@@ -19,9 +19,12 @@ typedef struct SG90_OOC {
     TIM_HandleTypeDef* TIMER;
     uint32_t TIM_Channel;
 
+    float prLastAngle;
+
     //functions
     float (*GetAngle2Pulse)(struct SG90_OOC *);
-    void (*SetAngle)(struct SG90_OOC *, float target_Angle);
+    void (*SetAngle_180)(struct SG90_OOC *, float target_Angle);
+    void (*SetAngle_270)(struct SG90_OOC *, float target_Angle);
     void (*Move2TargetAngle)(struct SG90_OOC *);
 } SG90;
 
@@ -30,15 +33,19 @@ SG90 SG90_Create();
 
 //OOP function define
 float getAngle2Pulse(struct SG90_OOC * SG90);
-void setAngle(struct SG90_OOC * SG90, float target_Angle);
+void setAngle_180(struct SG90_OOC * SG90, float target_Angle);
+void setAngle_270(struct SG90_OOC * SG90, float target_Angle);
+void SG90_scan(void);
 void move2TargetAngle(struct SG90_OOC* SG90);
-
+void SG90_move_pitch(struct SG90_OOC *SG90, float start_pitch, float end_pitch, float start_yaw, int time);
+void SG90_move_yaw(struct SG90_OOC *SG90, float start_yaw, float end_yaw, float start_pitch, int time);
+void setAngle_180_slowly(struct SG90_OOC *SG90, float target_Angle, int time);
+void setAngle_270_slowly(struct SG90_OOC *SG90, float target_Angle, int time);
 //Application functions
 void SG90_init();
 char* Click_Once(struct SG90_OOC * aSG90);
 char* Click_Num(struct SG90_OOC * aSG90, uint8_t Num);
 
-extern SG90 clickBox_1;
-extern SG90 clickBox_2;
-extern SG90 clickBox_3;
+extern SG90 Servo_pitch;
+extern SG90 Servo_yaw;
 #endif
